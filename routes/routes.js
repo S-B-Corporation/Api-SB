28515cbad68c07bebc0d5ff763dcf672246b8963
Carlosuca <00202118@uca.edu.sv>
2023-06-14 20:07:30 -0600

    add: delete user and product, restore password, improve credentials security

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,6 @@
 const { Router } = require("express")
 const bcrypt = require('bcrypt');
-const { body, validationResult, check } = require('express-validator');
+const { body, validationResult} = require('express-validator');
 const router = Router()
 const Product = require("../models/product")
 const User = require("../models/user")
@@ -10,17 +10,203 @@ const nodemailer = require('nodemailer');
 const { emailExist } = require("../helpers/db_validators")
 const Token = require("../models/tokens")
 const Categoria = require('../models/category');
+const generateVerificationCode = () => {
+  const code = Math.floor(1000 + Math.random() * 9000);
+  return code.toString();
+};
 
-router.get('/products', async (req, res) => {
+// Configura el transporte de Nodemailer
+const transporter = nodemailer.createTransport({
+  service: 'Gmail',
+  auth: {
+    user: 'sybmarketplace@gmail.com',
+    pass: 'ipphardrbiaxnhtp',
+  },
+});
+
+// Función para enviar el correo electrónico de recuperación de contraseña
+const sendPasswordResetEmail = (email, code) => {
+  const mailOptions = {
+    from: 'sybmarketplace@gmail.com',
+    to: email,
+    subject: 'Recuperación de contraseña',
+    text: `Tu código de verificación es: ${code}`,
+  };
+
+  return transporter.sendMail(mailOptions);
+};
+
+const emailVerificationCodes = {};
+
+router.post('/auth/register', [
+  body('email')
+    .isEmail().withMessage('El correo electrónico no es válido')
+    .custom(async (email) => {
+      await emailExist(email);
+    }).withMessage('Email already exist in the database'),
+], async (req, res) => {
   try {
-    const products = await product.find({});
+    const { name, password, email } = req.body;
+
+    const cliente = new User({ name, password, email });
+
+    const salt = bcrypt.genSaltSync();
+    cliente.password = bcrypt.hashSync(password, salt);
+
+    await cliente.save();
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const errorMessages = errors.array().map(error => error.msg);
+      return res.status(400).json({ msg: errorMessages });
+    }
+
+    const transporter = nodemailer.createTransport({
+      service: 'Gmail',
+      auth: {
+        user: 'sybmarketplace@gmail.com',
+        pass: 'ipphardrbiaxnhtp',
+      },
+    });
+    //pjtixaqiaptobhqc
+
+    const mailOptions = {
+      from: 'sybmarketplace@gmail.com',
+      to: email,
+      subject: 'Registro exitoso',
+      text: `¡Bienvenido, ${cliente.name}! Gracias por registrarte en nuestro sitio.`,
+    };
+
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.log(error);
+        res.status(500).send('Error al enviar el correo electrónico');
+      } else {
+        console.log('Correo electrónico enviado: ' + info.response);
+        res.status(201).json({ message: 'Usuario creado', cliente });
+      }
+    });
+  } catch (error) {
+    res.status(500).send('Error al agregar el cliente');
+  }
+});
+
+router.post('/auth/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Buscar el cliente por su correo electrónico en la base de datos
+    const cliente = await User.findOne({ email });
+
+    // Si no se encuentra el cliente, enviar una respuesta de error
+    if (!cliente) {
+      return res.status(404).json({ message: 'Cliente no encontrado' });
+    }
+
+    // Comparar la contraseña ingresada con la contraseña almacenada
+    const isPasswordValid = bcrypt.compareSync(password, cliente.password);
+
+    // Si las contraseñas no coinciden, enviar una respuesta de error
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: 'Usuario o contraseña incorrecta' });
+    }
+
+    // La contraseña es válida, iniciar sesión exitosamente
+    // Generar y enviar un token JWT
+    const token = await generateJWT(cliente._id);
+
+    // Crear una instancia del modelo Token
+    const tokenInstance = new Token({
+      token: token,
+      userId: cliente._id,
+    });
+
+    // Guardar la instancia en la base de datos
+    await tokenInstance.save();
+
+    // Actualizar el campo isWhitelisted a true
+    await Token.updateOne({ token: token }, { isWhitelisted: true });
+
+    res.status(200).json({ message: 'Inicio de sesión exitoso', token });
+  } catch (error) {
+    res.status(500).send('Error en el servidor');
+  }
+});
+
+router.post('/forgot-password', async (req, res) => {
+  const { email } = req.body;
+
+  try {
+    // Verificar si el correo electrónico existe en la base de datos
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: 'Correo electrónico no encontrado' });
+    }
+
+    // Generar un código de verificación único
+    const verificationCode = generateVerificationCode();
+
+    // Guardar el código de verificación en la base de datos para el usuario
+    emailVerificationCodes[email] = verificationCode;
+
+    // Enviar el correo electrónico de recuperación de contraseña
+    await sendPasswordResetEmail(email, verificationCode);
+
+    res.status(200).json({ message: 'Se ha enviado un correo electrónico de recuperación de contraseña' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error al enviar el correo electrónico de recuperación de contraseña' });
+  }
+});
+
+router.post('/restore-password', async (req, res) => {
+  try {
+    const { email, code, newPassword } = req.body;
+
+    // Obtener el código de verificación almacenado para el correo electrónico
+    const storedCode = emailVerificationCodes[email];
+
+    if (!storedCode) {
+      return res.status(404).json({ message: 'Código de verificación no encontrado' });
+    }
+
+    // Verificar si el código ingresado por el usuario coincide con el código almacenado
+    if (code !== storedCode) {
+      return res.status(400).json({ message: 'Código de verificación incorrecto' });
+    }
+
+    // Eliminar el código de verificación del objeto emailVerificationCodes
+    delete emailVerificationCodes[email];
+
+    // Actualizar la contraseña del usuario con la nueva contraseña
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: 'Usuario no encontrado' });
+    }
+
+    const salt = bcrypt.genSaltSync();
+    user.password = bcrypt.hashSync(newPassword, salt);
+    await user.save();
+
+    // Enviar una respuesta de éxito si la contraseña se actualiza correctamente
+    res.status(200).json({ message: 'Contraseña actualizada exitosamente' });
+  } catch (error) {
+    res.status(500).send('Error al verificar el código de verificación');
+  }
+});
+
+router.get('/allProducts', async (req, res) => {
+
+  try {
+    const products = await Product.find({});
     res.status(200).json(products);
   } catch (error) {
     res.status(500).send('Error al obtener los productos');
   }
 });
 
-router.get('/products/:id', async (req, res) => {
+router.get('/searchProducts/:id', async (req, res) => {
   try {
     const productId = req.params.id;
 
@@ -96,7 +282,7 @@ router.post('/categoria/add', async (req, res) => {
   }
 });
 
-router.get('/categorias', async (req, res) => {
+router.get('/allCategorias', async (req, res) => {
   const token = req.headers.authorization.split(' ')[1];
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
@@ -111,7 +297,7 @@ router.get('/categorias', async (req, res) => {
   }
 });
 
-router.get('/categorias/:nombreCategoria', async (req, res) => {
+router.get('/searchCategorias/:nombreCategoria', async (req, res) => {
   console.log(process.env.JWT_SECRET)
   const token = req.headers.authorization.split(' ')[1];
   try {
@@ -156,108 +342,12 @@ router.get('/cliente/productos', async (req, res) => {
   }
 });
 
-router.get('/clientes', async (req, res) => {
+router.get('/allClientes', async (req, res) => {
   try {
     const clientes = await User.find({});
     res.status(200).json(clientes);
   } catch (msg) {
     res.status(500).send('Error al obtener los clientes');
-  }
-});
-
-router.post('/clientes/add', [
-  body('email')
-    .isEmail().withMessage('El correo electrónico no es válido')
-    .custom(async (email) => {
-      await emailExist(email);
-    }).withMessage('Email already exist in the database'),
-], async (req, res) => {
-  try {
-    const { name, password, email } = req.body;
-
-    const cliente = new User({ name, password, email });
-
-    const salt = bcrypt.genSaltSync();
-    cliente.password = bcrypt.hashSync(password, salt);
-
-    await cliente.save();
-
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      const errorMessages = errors.array().map(error => error.msg);
-      return res.status(400).json({ msg: errorMessages });
-    }
-
-    const transporter = nodemailer.createTransport({
-      service: 'Gmail',
-      auth: {
-        user: 'sybmarketplace@gmail.com',
-        pass: 'ipphardrbiaxnhtp',
-      },
-    });
-    //pjtixaqiaptobhqc
-
-    const mailOptions = {
-      from: 'sybmarketplace@gmail.com',
-      to: email,
-      subject: 'Registro exitoso',
-      text: `¡Bienvenido, ${cliente.name}! Gracias por registrarte en nuestro sitio.`,
-    };
-
-
-    transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        console.log(error);
-        res.status(500).send('Error al enviar el correo electrónico');
-      } else {
-        console.log('Correo electrónico enviado: ' + info.response);
-        res.status(201).json({ message: 'Usuario creado', cliente });
-      }
-    });
-  } catch (error) {
-    res.status(500).send('Error al agregar el cliente');
-  }
-});
-
-router.post('/login', async (req, res) => {
-  try {
-    const { email, password } = req.body;
-
-    // Buscar el cliente por su correo electrónico en la base de datos
-    const cliente = await User.findOne({ email });
-
-    // Si no se encuentra el cliente, enviar una respuesta de error
-    if (!cliente) {
-      return res.status(404).json({ message: 'Cliente no encontrado' });
-    }
-
-    // Comparar la contraseña ingresada con la contraseña almacenada
-    const isPasswordValid = bcrypt.compareSync(password, cliente.password);
-
-    // Si las contraseñas no coinciden, enviar una respuesta de error
-    if (!isPasswordValid) {
-      return res.status(401).json({ message: 'Usuario o contraseña incorrecta' });
-    }
-
-    // La contraseña es válida, iniciar sesión exitosamente
-    // Generar y enviar un token JWT
-    const token = await generateJWT(cliente._id);
-
-    // Crear una instancia del modelo Token
-    const tokenInstance = new Token({
-      token: token,
-      userId: cliente._id,
-    });
-
-    // Guardar la instancia en la base de datos
-    await tokenInstance.save();
-
-    // Actualizar el campo isWhitelisted a true
-    await Token.updateOne({ token: token }, { isWhitelisted: true });
-
-    res.status(200).json({ message: 'Inicio de sesión exitoso', token });
-  } catch (error) {
-    res.status(500).send('Error en el servidor');
   }
 });
 
@@ -298,6 +388,63 @@ router.post('/logout', async (req, res) => {
 
     // En caso de que ocurra un error al verificar el token, enviar una respuesta de error
     res.status(401).json({ message: 'Error al realizar el logout' });
+  }
+});
+
+router.delete('/delete/productos/cliente/:id', async (req, res) => {
+  const token = req.headers.authorization.split(' ')[1];
+  const productId = req.params.id;
+
+  try {
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    // Obtener el ID del usuario del token decodificado
+    const idcliente = decoded._id;
+    // Buscar el producto por su ID
+    const product = await Product.findById(productId);
+
+    // Verificar si el producto existe
+    if (!product) {
+      return res.status(404).json({ message: 'Producto no encontrado' });
+    }
+
+    // Realizar la eliminación del producto
+    await product.deleteOne();
+
+    res.status(200).json({ message: 'Producto eliminado correctamente' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error al eliminar el producto' });
+  }
+});
+
+router.delete('/delete/usuarios/:id', async (req, res) => {
+  const token = req.headers.authorization.split(' ')[1];
+  const userId = req.params.id;
+
+  try {
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    // Obtener el ID del usuario del token decodificado
+    const idcliente = decoded._id;
+
+    // Buscar el usuario por su ID
+    const user = await User.findById(userId);
+
+    // Verificar si el usuario existe
+    if (!user) {
+      return res.status(404).json({ message: 'Usuario no encontrado' });
+    }
+
+    // Realizar la eliminación del usuario
+    await user.deleteOne();
+
+    res.status(200).json({ message: 'Usuario eliminado correctamente' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error al eliminar el usuario' });
   }
 });
 
